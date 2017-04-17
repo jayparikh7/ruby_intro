c987c75ffbadd2b70eca07131cb9464f186cdf41
Jay Parikh <jayparikh7@users.noreply.github.com>
2017-04-17 00:41:05 -0500

    Changes based on Intro Readings

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -2,7 +2,28 @@ class ProgramsController < ApplicationController
   def home
     # Your code goes below.
 
-    @your_output = "Replace this string with your output"
+    rock = 1
+    throw_down = rand(3)
+    selection = "Throw Down"
+    outcome = "Not Yet"
+
+    if throw_down == 0
+      selection = "scissor"
+    elsif throw_down == 1
+      selection = "rock"
+    else
+      selection = "paper"
+    end
+
+    if throw_down < rock
+      outcome = "Fatality - You Lose"
+    elsif throw_down == rock
+      outcome = "Lame - Tie"
+    else
+      outcome = "Yay - Win!"
+    end
+
+    @your_output = selection + " " + outcome
 
     render("programs/home.html.erb")
   end
@@ -18,17 +39,37 @@ class ProgramsController < ApplicationController
   def second_program
     # Your code goes below.
 
-    @your_output = "Replace this string with your output"
+    our_numbers = [4, 5, 10]
+    squared_numbers = []
+
+    our_numbers.each do |num|
+      square = num * num
+      squared_numbers.push(square)
+
+    end
+
+    @your_output = squared_numbers.sum
 
     render("programs/second_program.html.erb")
   end
 
   def third_program
     numbers = (1..999).to_a
+    divide_by_three = []
+    divide_by_five = []
 
-    # Your code goes below.
+    numbers.each do |counter|
+      if counter % 3 == 0
+        divide_by_three.push(counter)
+      end
+      if counter % 5 == 0
+        if counter % 3 != 0
+          divide_by_five.push(counter)
+        end
+      end
+    end
 
-    @your_output = "Replace this string with your output"
+    @your_output = divide_by_three.sum + divide_by_five.sum
 
     render("programs/third_program.html.erb")
   end
